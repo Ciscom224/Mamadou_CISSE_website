9503b3ca6b452a3184f7b7e77667bc6176a88946
Mamadou Cissé <mamadouibncisse@gmail.com>
2023-01-11 04:07:17 +0100

    homme

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,12 +1,29 @@
 import React from "react";
+import bg from '../assets/me.png'
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/home.css'
+import { Button } from "bootstrap";
+import NavScrollExample from "./components/navbar";
 export default function Home() {
   return (
-    <div className="home-container">
+    <div className="container-fuild">
+      <div className="row home-screen">
+        <div className="col-md-4 left-part">
+          <div className="btns">
+            <button className="bt bt_resume">Get Resume</button>
+            <button className="bt btn-hide">Hide Me</button>
+          </div>
+          <img src={bg}/>
+        </div>
+        <div className="col-md-8">
+          <NavScrollExample/>
+          {/* <h1>deifilement des les </h1> */}
+        </div>
+      </div>
       
-      <img src="./src/assets/me.jpg"/>
-      <h1>Bonjour le monde</h1>
+      
+      
     </div>
   );
 }
